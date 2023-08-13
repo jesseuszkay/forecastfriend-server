cef38723b6a2798e76ab960bace752f5b0d41ec9
Jesse Uszkay <jesseuszkay@gmail.com>
2023-08-12 21:30:29 -0400

    add save weather snapshot functionality

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("weather.db");
 const axios = require("axios");
 const cors = require("cors");
 const PORT = 8080;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/weather", (req, res) => {
+db.run(`
+  CREATE TABLE IF NOT EXISTS weather_snapshots (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    temperature REAL,
+    weathercode INTEGER,
+    timestamp TEXT
+  )
+`);
+
+app.get("/weather", (_req, res) => {
   axios
     .get(
       "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
@@ -28,7 +40,7 @@ app.get("/weather", (req, res) => {
     });
 });
 
-app.get("/historical", (req, res) => {
+app.get("/historical", (_req, res) => {
   axios
     .get(
       "https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=GMT&past_days=5"
@@ -40,6 +52,31 @@ app.get("/historical", (req, res) => {
     .catch((error) => {
       console.log(error);
     });
+});
+
+app.get("/snapshots", (_req, res) => {
+  const query =
+    "SELECT * FROM weather_snapshots ORDER BY timestamp DESC LIMIT 5";
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to retrieve readings" });
+    }
+    return res.status(200).json(rows);
+  });
+});
+
+app.post("/snapshots", (req, res) => {
+  const { temperature, weathercode, timestamp } = req.body;
+  const query =
+    "INSERT INTO weather_snapshots (temperature, weathercode, timestamp) VALUES (?, ?, ?)";
+
+  db.run(query, [temperature, weathercode, timestamp], (err) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to store reading" });
+    }
+    return res.status(200).json({ message: "Reading stored successfully" });
+  });
 });
 
 app.listen(PORT, () => {
